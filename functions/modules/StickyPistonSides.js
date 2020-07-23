122fc842c69e44544d94305b2a1c30f5a0d3513e
Brendan Leu <banakin9@gmail.com>
2020-07-23 19:48:34 -0400

    quick fix for sticky pistons

--- functions/modules/StickyPistonSides.js
+++ functions/modules/StickyPistonSides.js
@@ -69,18 +69,18 @@ module.exports = {
                     }
                 }`,
             },
-            {
-                inPackName: "sticky_piston_inventory.json",
-                path: "assets/minecraft/models/block",
-                data: `{
-                    "parent": "block/template_piston",
-                    "textures": {
-                        "bottom": "block/piston_bottom",
-                        "side": "block/piston_side_sticky",
-                        "platform": "block/piston_top_sticky"
-                    }
-                }`,
-            },
+            // {
+            //     inPackName: "sticky_piston_inventory.json",
+            //     path: "assets/minecraft/models/block",
+            //     data: `{
+            //         "parent": "block/template_piston",
+            //         "textures": {
+            //             "bottom": "block/piston_bottom",
+            //             "side": "block/piston_side_sticky",
+            //             "platform": "block/piston_top_sticky"
+            //         }
+            //     }`,
+            // },
             {
                 inPackName: "template_piston_head_short_sticky.json",
                 path: "assets/minecraft/models/block",
@@ -124,37 +124,37 @@ module.exports = {
                     ]
                 }`,
             },
-            {
-                inPackName: "sticky_piston.json",
-                path: "assets/minecraft/models/item",
-                data: `{
-                    "textures": {
-                        "particle": "#platform"
-                    },
-                    "elements": [
-                        {   "from": [ 0, 0, 0 ],
-                            "to": [ 16, 16, 4 ],
-                            "faces": {
-                                "down":  { "uv": [ 0, 0, 16,  4 ], "texture": "#side", "cullface": "down", "rotation": 180 },
-                                "up":    { "uv": [ 0, 0, 16,  4 ], "texture": "#side", "cullface": "up" },
-                                "north": { "uv": [ 0, 0, 16, 16 ], "texture": "#platform", "cullface": "north" },
-                                "south": { "uv": [ 0, 0, 16, 16 ], "texture": "#unsticky" },
-                                "west":  { "uv": [ 0, 0, 16,  4 ], "texture": "#side", "rotation": 270, "cullface": "west" },
-                                "east":  { "uv": [ 0, 0, 16,  4 ], "texture": "#side", "rotation": 90, "cullface": "east" }
-                            }
-                        },
-                        {   "from": [ 6, 6, 4 ],
-                            "to": [ 10, 10, 20 ],
-                            "faces": {
-                                "down": { "uv": [  0, 0, 16, 4 ], "texture": "#unsticky_side", "rotation": 90 },
-                                "up":   { "uv": [  0, 0, 16, 4 ], "texture": "#unsticky_side", "rotation": 270 },
-                                "west": { "uv": [ 16, 4,  0, 0 ], "texture": "#unsticky_side" },
-                                "east": { "uv": [  0, 0, 16, 4 ], "texture": "#unsticky_side" }
-                            }
-                        }
-                    ]
-                }`,
-            },
+            // {
+            //     inPackName: "sticky_piston.json",
+            //     path: "assets/minecraft/models/item",
+            //     data: `{
+            //         "textures": {
+            //             "particle": "#platform"
+            //         },
+            //         "elements": [
+            //             {   "from": [ 0, 0, 0 ],
+            //                 "to": [ 16, 16, 4 ],
+            //                 "faces": {
+            //                     "down":  { "uv": [ 0, 0, 16,  4 ], "texture": "#side", "cullface": "down", "rotation": 180 },
+            //                     "up":    { "uv": [ 0, 0, 16,  4 ], "texture": "#side", "cullface": "up" },
+            //                     "north": { "uv": [ 0, 0, 16, 16 ], "texture": "#platform", "cullface": "north" },
+            //                     "south": { "uv": [ 0, 0, 16, 16 ], "texture": "#unsticky" },
+            //                     "west":  { "uv": [ 0, 0, 16,  4 ], "texture": "#side", "rotation": 270, "cullface": "west" },
+            //                     "east":  { "uv": [ 0, 0, 16,  4 ], "texture": "#side", "rotation": 90, "cullface": "east" }
+            //                 }
+            //             },
+            //             {   "from": [ 6, 6, 4 ],
+            //                 "to": [ 10, 10, 20 ],
+            //                 "faces": {
+            //                     "down": { "uv": [  0, 0, 16, 4 ], "texture": "#unsticky_side", "rotation": 90 },
+            //                     "up":   { "uv": [  0, 0, 16, 4 ], "texture": "#unsticky_side", "rotation": 270 },
+            //                     "west": { "uv": [ 16, 4,  0, 0 ], "texture": "#unsticky_side" },
+            //                     "east": { "uv": [  0, 0, 16, 4 ], "texture": "#unsticky_side" }
+            //                 }
+            //             }
+            //         ]
+            //     }`,
+            // },
         ]
     },
     format321: {
@@ -253,18 +253,18 @@ module.exports = {
                     }
                 }`,
             },
-            {
-                inPackName: "piston_inventory_sticky.json",
-                path: "assets/minecraft/models/block",
-                data: `{
-                    "parent": "block/cube_bottom_top",
-                    "textures": {
-                        "bottom": "blocks/piston_bottom",
-                        "side": "blocks/piston_side_sticky",
-                        "top": "blocks/piston_top_sticky"
-                    }
-                }`,
-            },
+            // {
+            //     inPackName: "piston_inventory_sticky.json",
+            //     path: "assets/minecraft/models/block",
+            //     data: `{
+            //         "parent": "block/cube_bottom_top",
+            //         "textures": {
+            //             "bottom": "blocks/piston_bottom",
+            //             "side": "blocks/piston_side_sticky",
+            //             "top": "blocks/piston_top_sticky"
+            //         }
+            //     }`,
+            // },
             {
                 inPackName: "sticky_piston.json",
                 path: "assets/minecraft/models/block",
