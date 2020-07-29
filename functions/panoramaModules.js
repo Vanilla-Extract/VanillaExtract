9f4dae6681dd8a912191be8e7018c8cddd57e11e
Brendan Leu <banakin9@gmail.com>
2020-07-28 20:46:13 -0400

    fix panoramas

--- functions/panoramaModules.js
+++ functions/panoramaModules.js
@@ -13,19 +13,47 @@ const addModules = async function(moduleName, archive, bucket){
 
     // If it exists
     if (folders[moduleName] !== undefined && folders[moduleName] !== null) {
-        const promises = [];
+        // Image 0
+        await bucket.file(path.join("packfiles", packFilesPath, folders[moduleName], '0.png')).download().then((data) => {
+            const contents = data[0];
+            archive.append(contents, {name: path.join(inPackPath, 'panorama_0.png')});
+            return;
+        });
 
-        // For all 5 images
-        for (let i = 0; i <= 5; i++) {
-            promises.push(
-                bucket.file(path.join("packfiles", packFilesPath, folders[moduleName], i.toString()+'.png')).download().then((data) => {
-                    const contents = data[0];
-                    archive.append(contents, {name: path.join(inPackPath, 'panorama_'+i.toString()+'.png')});
-                    return;
-                })
-            );
-        }
-        await Promise.all(promises);
+        // Image 1
+        await bucket.file(path.join("packfiles", packFilesPath, folders[moduleName], '1.png')).download().then((data) => {
+            const contents = data[0];
+            archive.append(contents, {name: path.join(inPackPath, 'panorama_1.png')});
+            return;
+        });
+        
+        // Image 2
+        await bucket.file(path.join("packfiles", packFilesPath, folders[moduleName], '2.png')).download().then((data) => {
+            const contents = data[0];
+            archive.append(contents, {name: path.join(inPackPath, 'panorama_2.png')});
+            return;
+        });
+        
+        // Image 3
+        await bucket.file(path.join("packfiles", packFilesPath, folders[moduleName], '3.png')).download().then((data) => {
+            const contents = data[0];
+            archive.append(contents, {name: path.join(inPackPath, 'panorama_3.png')});
+            return;
+        });
+        
+        // Image 4
+        await bucket.file(path.join("packfiles", packFilesPath, folders[moduleName], '4.png')).download().then((data) => {
+            const contents = data[0];
+            archive.append(contents, {name: path.join(inPackPath, 'panorama_4.png')});
+            return;
+        });
+        
+        // Image 5
+        await bucket.file(path.join("packfiles", packFilesPath, folders[moduleName], '5.png')).download().then((data) => {
+            const contents = data[0];
+            archive.append(contents, {name: path.join(inPackPath, 'panorama_5.png')});
+            return;
+        });
     }
     return;
 };
