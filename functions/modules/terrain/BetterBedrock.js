b185d68f4c560a41bcb41a480e2f30c362eb45ea
Brendan Leu <banakin9@gmail.com>
2020-08-28 13:50:53 -0400

    terrain

--- functions/modules/terrain/BetterBedrock.js
+++ functions/modules/terrain/BetterBedrock.js
@@ -1,29 +1,13 @@
-const path = require('path');
-
-// Module Data
-const moduleData = {
-    packFilesPath: "modules/terrain/BetterBedrock/",
-    name: "BetterBedrock.png",
-    inPackName: "bedrock.png",
-    path654: "assets/minecraft/textures/block",
-    path321: "assets/minecraft/textures/blocks",
+// File Paths
+module.exports = {
+    "1.16.2":  "/modules/terrain/BetterBedrock/1.13-1.16.2/",
+    "1.16":    "/modules/terrain/BetterBedrock/1.13-1.16.2/",
+    "1.15":    "/modules/terrain/BetterBedrock/1.13-1.16.2/",
+    "1.14":    "/modules/terrain/BetterBedrock/1.13-1.16.2/",
+    "1.13":    "/modules/terrain/BetterBedrock/1.13-1.16.2/",
+    "1.12":    "/modules/terrain/BetterBedrock/1.8-1.12/",
+    "1.11":    "/modules/terrain/BetterBedrock/1.8-1.12/",
+    "1.10":    "/modules/terrain/BetterBedrock/1.8-1.12/",
+    "1.9":     "/modules/terrain/BetterBedrock/1.8-1.12/",
+    "1.8":     "/modules/terrain/BetterBedrock/1.8-1.12/",
 };
-
-// Module function
-module.exports = async function(format, archive, bucket){
-    // Change data based on format
-    let pathData;
-    if (format === 1 || format === 2 || format === 3) {
-        pathData = moduleData.path321;
-    } else if (format === 4 || format === 5 || format === 6) {
-        pathData = moduleData.path654;
-    } else {
-        console.log('format not addressed');
-        return;
-    }
-
-    // Add file
-    await bucket.file(path.join("packfiles", moduleData.packFilesPath, moduleData.name)).download().then((data) => {
-        return archive.append(data[0], {name: path.join(pathData, moduleData.inPackName)});
-    });
-}
