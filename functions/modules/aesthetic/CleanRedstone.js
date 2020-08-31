d67bebffc0aa15664218950fbcfce77f4e87bfe3
Brendan Leu <banakin9@gmail.com>
2020-08-31 14:23:16 -0400

    Add JavaScript for modules

---
+++ functions/modules/aesthetic/CleanRedstone.js
@@ -0,0 +1,8 @@
+// File Paths
+module.exports = {
+    "1.16.2":  "/modules/aesthetic/CleanRedstone/1.13-latest/",
+    "1.16":    "/modules/aesthetic/CleanRedstone/1.13-latest/",
+    "1.15":    "/modules/aesthetic/CleanRedstone/1.13-latest/",
+    "1.14":    "/modules/aesthetic/CleanRedstone/1.13-latest/",
+    "1.13":    "/modules/aesthetic/CleanRedstone/1.13-latest/",
+};
---
+++ functions/modules/utility/NetherwartGrowthStage.js
@@ -0,0 +1,8 @@
+// File Paths
+module.exports = {
+    "1.16.2":  "/modules/utility/NetherwartGrowthStage/1.13-latest/",
+    "1.16":    "/modules/utility/NetherwartGrowthStage/1.13-latest/",
+    "1.15":    "/modules/utility/NetherwartGrowthStage/1.13-latest/",
+    "1.14":    "/modules/utility/NetherwartGrowthStage/1.13-latest/",
+    "1.13":    "/modules/utility/NetherwartGrowthStage/1.13-latest/",
+};
