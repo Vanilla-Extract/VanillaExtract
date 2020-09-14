6117a97595037176857672815d1ce77f9380c40d
Sei <seirin-blu@users.noreply.github.com>
2020-09-14 10:32:27 -0700

    Added Glass Doors

---
+++ functions/src/modules/aesthetic/GlassDoors.js
@@ -0,0 +1,7 @@
+// File Paths
+module.exports = {
+    "1.16.3":  "/modules/aesthetic/GlassDoors/1.15-latest/",
+    "1.16.2":  "/modules/aesthetic/GlassDoors/1.15-latest/",
+    "1.16":    "/modules/aesthetic/GlassDoors/1.15-latest/",
+    "1.15":    "/modules/aesthetic/GlassDoors/1.15-latest/",
+};
