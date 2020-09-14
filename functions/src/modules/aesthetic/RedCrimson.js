cc975937aa220048df5ed86956a178386f4b268d
Sei <seirin-blu@users.noreply.github.com>
2020-09-14 10:16:22 -0700

    Added Crimsoner Crimson (thats a dumb name)

---
+++ functions/src/modules/aesthetic/RedCrimson.js
@@ -0,0 +1,7 @@
+// File Paths
+module.exports = {
+    "1.16.3":  "/modules/aesthetic/RedCrimson/1.15-latest/",
+    "1.16.2":  "/modules/aesthetic/RedCrimson/1.15-latest/",
+    "1.16":    "/modules/aesthetic/RedCrimson/1.15-latest/",
+    "1.15":    "/modules/aesthetic/RedCrimson/1.15-latest/",
+};
