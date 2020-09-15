ea940691274b964bbd2234ea524b21e1159edd00
Sei <seirin-blu@users.noreply.github.com>
2020-09-15 10:45:54 -0700

    added Colored Enchanting Particles and fixed a few packs

--- functions/src/modules/aesthetic/GlassDoors.js
+++ functions/src/modules/aesthetic/GlassDoors.js
@@ -1,6 +1,5 @@
 // File Paths
 module.exports = {
-    "1.16.3":  "/modules/aesthetic/GlassDoors/1.15-latest/",
     "1.16.2":  "/modules/aesthetic/GlassDoors/1.15-latest/",
     "1.16":    "/modules/aesthetic/GlassDoors/1.15-latest/",
     "1.15":    "/modules/aesthetic/GlassDoors/1.15-latest/",
