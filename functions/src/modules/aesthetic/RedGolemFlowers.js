648acdb062917255fa72b4edc8308795484be72f
Sei <seirin-blu@users.noreply.github.com>
2020-09-15 13:01:53 -0700

    fixed dumb mistake

--- functions/src/modules/aesthetic/RedGolemFlowers.js
+++ functions/src/modules/aesthetic/RedGolemFlowers.js
@@ -1,3 +1,11 @@
+// FORMAT REFERENCE
+// 6 = 1.16.2
+// 5 = 1.15-1.16
+// 4 = 1.13-1.14
+// 3 = 1.11-1.12
+// 2 = 1.9-1.10
+// 1 = 1.8
+
 // File Paths
 module.exports = {
     "1.16.2":  "/modules/aesthetic/RedGolemFlowers/1.15-latest/",
