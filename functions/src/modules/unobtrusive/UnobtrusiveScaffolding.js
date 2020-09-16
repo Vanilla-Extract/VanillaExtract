eb1fc870551b7ee05175677f8c075a846759e652
Sei <seirin-blu@users.noreply.github.com>
2020-09-15 17:18:26 -0700

    Fixed dumb bug

--- functions/src/modules/unobtrusive/UnobtrusiveScaffolding.js
+++ functions/src/modules/unobtrusive/UnobtrusiveScaffolding.js
@@ -1,6 +1,6 @@
 module.exports = {
-    "1.16.2":  "/modules/unobtrusive/ReducedPumpkinBlur/1.14-latest/",
-    "1.16":    "/modules/unobtrusive/ReducedPumpkinBlur/1.14-latest/",
-    "1.15":    "/modules/unobtrusive/ReducedPumpkinBlur/1.14-latest/",
-    "1.14":    "/modules/unobtrusive/ReducedPumpkinBlur/1.14-latest/",
+    "1.16.2":  "/modules/unobtrusive/UnobtrusiveScaffolding/1.14-latest/",
+    "1.16":    "/modules/unobtrusive/UnobtrusiveScaffolding/1.14-latest/",
+    "1.15":    "/modules/unobtrusive/UnobtrusiveScaffolding/1.14-latest/",
+    "1.14":    "/modules/unobtrusive/UnobtrusiveScaffolding/1.14-latest/",
 };
