b654d333a693bf16c155991234314be48337c21b
Sei <seirin-blu@users.noreply.github.com>
2020-09-18 01:21:38 -0700

    Fixed js file

--- functions/src/modules/terrain/PebblelessDirt copy.js
+++
@@ -1,8 +0,0 @@
-// File Paths
-module.exports = {
-    "1.16.2":  "/modules/terrain/PebblelessDirt/1.16-latest/",
-    "1.16":    "/modules/terrain/PebblelessDirt/1.16-latest/",
-    "1.15":    "/modules/terrain/PebblelessDirt/1.13-1.15/",
-    "1.14":    "/modules/terrain/PebblelessDirt/1.13-1.15/",
-    "1.13":    "/modules/terrain/PebblelessDirt/1.13-1.15/",
-};
