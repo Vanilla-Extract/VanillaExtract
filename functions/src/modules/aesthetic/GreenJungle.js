1bfe3fa78ba76ebdb932452068ffedf356134288
Sei <seirin-blu@users.noreply.github.com>
2020-09-17 21:43:05 -0700

    Added Green Jungle Wood, Softer Wool, and Unique Dyes

---
+++ functions/src/modules/aesthetic/GreenJungle.js
@@ -0,0 +1,16 @@
+// FORMAT REFERENCE
+// 6 = 1.16.2
+// 5 = 1.15-1.16
+// 4 = 1.13-1.14
+// 3 = 1.11-1.12
+// 2 = 1.9-1.10
+// 1 = 1.8
+
+// File Paths
+module.exports = {
+    "1.16.2":  "/modules/aesthetic/GreenJungle/1.13-latest/",
+    "1.16":    "/modules/aesthetic/GreenJungle/1.13-latest/",
+    "1.15":    "/modules/aesthetic/GreenJungle/1.13-latest/",
+    "1.14":    "/modules/aesthetic/GreenJungle/1.13-latest/",
+    "1.13":    "/modules/aesthetic/GreenJungle/1.13-latest/",
+};
