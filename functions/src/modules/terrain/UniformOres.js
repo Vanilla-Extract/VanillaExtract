389388523b56a0d9f255c0850563e1d44053d835
Sei <seirin-blu@users.noreply.github.com>
2020-09-18 01:21:52 -0700

    Fixed JS file

---
+++ functions/src/modules/terrain/UniformOres.js
@@ -0,0 +1,8 @@
+// File Paths
+module.exports = {
+    "1.16.2":  "/modules/terrain/UniformOres/1.16-latest/",
+    "1.16":    "/modules/terrain/UniformOres/1.16-latest/",
+    "1.15":    "/modules/terrain/UniformOres/1.13-1.15/",
+    "1.14":    "/modules/terrain/UniformOres/1.13-1.15/",
+    "1.13":    "/modules/terrain/UniformOres/1.13-1.15/",
+};
